c182839d29a690781986c177c7ed67ce82bdfa17
Wiltse90 <Wiltse90@gmail.com>
2021-05-14 23:59:29 -0500

    Update Main.java

--- CSC422assignment1Pt2/src/main/java/Main.java
+++ CSC422assignment1Pt2/src/main/java/Main.java
@@ -1,13 +1,16 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Steven Wiltse
+ * Assignment 1, Part 2
+ * Concordia St. Paul - SUMMER 100 CSC 422 Software Engineering
+ * 
+ * written: 05/12/2021
+ * revised: 05/14/2021
  */
 
-/**
- *
- * @author Steven
- */
+ /**
+  * Pet database
+  */
+
 import java.util.Scanner; 
 
 public class Main {
@@ -16,9 +19,22 @@ public class Main {
   while(run){
     Scanner select = new Scanner(System.in);  // Create a Scanner object
     System.out.println("What would you like to do?");
-
-    String userName = select.nextLine();  // Read user input
-    System.out.println("Selection is: " + userName);
+    System.out.println("1) View all pets");
+    System.out.println("2) Add more pets");
+    System.out.println("3) Update an existing pet");
+    System.out.println("4) Remove an existing pet");
+    System.out.println("5) Search pets by name");
+    System.out.println("6) Search pets by age");
+    System.out.println("7) Exit program");
+    
+    
+    int userChoice = select.nextInt();  // Read user input
+    System.out.println("Your choice: " + userChoice);
+    
+    if (userChoice == 7){
+        run = false;
+        System.out.println("Your choice: " + userChoice + " Goodbye!");
+    }
   }
   }
 }
